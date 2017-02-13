2c165e02c9f8dbe8052d3e192761e9ce862aaad9
Chris Berry <Chris Berry>
2017-02-13 20:15:46 +0000

    Added contact info to header

--- header.php
+++ header.php
@@ -18,6 +18,30 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+	<link rel="apple-touch-icon" sizes="57x57" href="http://chrisbdev.com/Content/Favicon/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="http://chrisbdev.com/Content/Favicon/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="http://chrisbdev.com/Content/Favicon/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="http://chrisbdev.com/Content/Favicon/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="http://chrisbdev.com/Content/Favicon/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="http://chrisbdev.com/Content/Favicon/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="http://chrisbdev.com/Content/Favicon/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="http://chrisbdev.com/Content/Favicon/apple-touch-icon-152x152.png">
+    <link rel="icon" type="image/png" href="http://chrisbdev.com/Content/Favicon/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="http://chrisbdev.com/Content/Favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="http://chrisbdev.com/Content/Favicon/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="http://chrisbdev.com/Content/Favicon/favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="http://chrisbdev.com/Content/Favicon/favicon-128.png" sizes="128x128" />
+    <meta name="application-name" content="&nbsp;" />
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="http://chrisbdev.com/Content/Favicon/mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="http://chrisbdev.com/Content/Favicon/mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="http://chrisbdev.com/Content/Favicon/mstile-150x150.png" />
+    <meta name="msapplication-wide310x150logo" content="http://chrisbdev.com/Content/Favicon/mstile-310x150.png" />
+    <meta name="msapplication-square310x310logo" content="http://chrisbdev.com/Content/Favicon/mstile-310x310.png" />
+
+
+
 	<?php wp_head();?>
 
 	<script>
@@ -42,8 +66,11 @@
 			</div>
 		
 			<h1 class="c-header__main-heading c-header__item">
-				Chris Berry - Front end developer
+				Chris Berry
 			</h1>
+
+			
+			<a class="c-header__contact" href="mailto:chrisberry4545@googlemail.com">chrisberry4545@googlemail.com</a>
 			
 			<!--<img src="<?php bloginfo( 'template_url' );?>/img/GitHub-Mark-32px"/>-->
 		</div>
