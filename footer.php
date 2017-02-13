909dddc03f1daff583cce6939756aa7498fb3268
Chris Berry <Chris Berry>
2017-02-13 20:59:09 +0000

    Revising code highlighting to work with highlightjs

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
         </div> <!-- /.container -->
     
-        
 	      <script src="<?php bloginfo( 'template_directory' );?>/scripts/cutoutname.js" />
         <?php wp_footer(); ?> 
   </body>
